d19faeb67cd4bb6ee3057fdf19bba5b66936d87c
bietdoikiem <minhlucky2408@gmail.com>
2022-04-06 00:12:21 +0700

    feat: add Bottom-Up DP solution for Leetcode no. 1143

--- leetcode/1143_Longest_Common_Subsequence/main.cpp
+++ leetcode/1143_Longest_Common_Subsequence/main.cpp
@@ -25,11 +25,12 @@ void printArr(T const (&x)[SIZE]) {
 }
 
 int dp[1000][1000];
+int dp2[1001][1001];  // Cache for Bottom-Up approach
 
 int max(int x, int y) { return x > y ? x : y; }
 
-int helper(string text1, string text2, size_t n, size_t m, size_t i, size_t j) {
-  if (i >= text1.size() || j >= text2.size()) {
+int helper(string text1, string text2, size_t m, size_t n, size_t i, size_t j) {
+  if (i == m || j == n) {
     return 0;
   }
   if (dp[i][j] != -1) {
@@ -44,25 +45,46 @@ int helper(string text1, string text2, size_t n, size_t m, size_t i, size_t j) {
   return dp[i][j];
 }
 
+// NOTE: TLE flagged in Leetcode submission
 int longestCommonSubsequenceMemoization(string text1, string text2) {
   memset(dp, -1, sizeof(dp));  // Set all to -1
   int ans = helper(text1, text2, text1.size(), text2.size(), 0, 0);
   return ans;
 }
 
+int longestCommonSubsequence(string text1, string text2) {
+  size_t m = text1.size();
+  size_t n = text2.size();
+  for (size_t i = m - 1; i < m; i--) {
+    for (size_t j = n - 1; j < n; j--) {
+      // cout << "i: " << i << " - j: " << j << endl;
+      if (text1[i] == text2[j]) {
+        // cout << "Did it run here?" << endl;
+        dp2[i][j] = 1 + dp2[i + 1][j + 1];
+      } else {
+        // cout << "Did it run again?" << endl;
+        dp2[i][j] = max(dp2[i + 1][j], dp2[i][j + 1]);
+      }
+    }
+  }
+  return dp2[0][0];
+}
+
 int main() {
   // TODO: Solve the problem!
-  string text1 = "abcde";
-  string text2 = "ace";
-  cout << "LCS of text1 and text1 is: "
-       << longestCommonSubsequenceMemoization(text1, text2) << endl;
+  string text1 = "quetzalcoatl";
+  string text2 = "tezcatlipoca";
+  cout << "LCS of text1 and text2 is: "
+       << longestCommonSubsequence(text1, text2) << endl;
   text1 = "abc";
   text2 = "abc";
-  cout << "LCS of text1 and text1 is: "
-       << longestCommonSubsequenceMemoization(text1, text2) << endl;
+  memset(dp2, 0, sizeof(dp2));  // Reset DP cache
+  cout << "LCS of text1 and text2 is: "
+       << longestCommonSubsequence(text1, text2) << endl;
   text1 = "abc";
   text2 = "def";
-  cout << "LCS of text1 and text1 is: "
-       << longestCommonSubsequenceMemoization(text1, text2) << endl;
+  memset(dp2, 0, sizeof(dp2));
+  cout << "LCS of text1 and text2 is: "
+       << longestCommonSubsequence(text1, text2) << endl;
   return 0;
 }
