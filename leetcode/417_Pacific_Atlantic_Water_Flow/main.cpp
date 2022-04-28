5157d6b2a6bd2b80ebc5788abdd4038f21b62c51
bietdoikiem <intern.minhnpq@gmail.com>
2022-04-28 17:41:28 +0700

    feat: add DFS solution for Leetcode no. 417

--- leetcode/417_Pacific_Atlantic_Water_Flow/main.cpp
+++ leetcode/417_Pacific_Atlantic_Water_Flow/main.cpp
@@ -45,12 +45,6 @@ bool isValid(vector<vector<bool>>& V, const int& R, const int& C, int x,
   return true;
 }
 
-bool adjToPacific(int x, int y) { return x == 0 || y == 0; }
-
-bool adjToAltantic(int x, int y, const int& R, const int& C) {
-  return x == R - 1 || y == C - 1;
-}
-
 void bfs(vector<vector<int>>& heights, queue<pair<int, int>>& q,
          vector<vector<bool>>& V, const int& R, const int& C, int sX, int sY) {
   q.push({sX, sY});
@@ -80,16 +74,12 @@ vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
   vector<bool> col(C, false);
   vector<vector<bool>> vPac(R, col), vAlt(R, col);
   for (int i = 0; i < R; i++) {
-    for (int j = 0; j < C; j++) {
-      if (adjToPacific(i, j)) {
-        qPac.push({i, j});
-        vPac[i][j] = true;
-      }
-      if (adjToAltantic(i, j, R, C)) {
-        qAlt.push({i, j});
-        vAlt[i][j] = true;
-      }
-    }
+    vPac[i][0] = true, vAlt[i][C - 1] = true;
+    qPac.push({i, 0}), qAlt.push({i, C - 1});
+  }
+  for (int i = 0; i < C; i++) {
+    vPac[0][i] = true, vAlt[R - 1][i] = true;
+    qPac.push({0, i}), qAlt.push({R - 1, i});
   }
   bfs(heights, qPac, vPac, R, C, 0, 0);
   bfs(heights, qAlt, vAlt, R, C, R - 1, C - 1);
@@ -101,20 +91,63 @@ vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
   return ans;
 }
 
+/* * DFS Version * */
+
+void dfs(vector<vector<int>>& heights, vector<vector<bool>>& V, const int& R,
+         const int& C, int x, int y) {
+  V[x][y] = true;
+  for (int i = 0; i < 4; i++) {
+    int nx = x + dX[i];
+    int ny = y + dY[i];
+    if (isValid(V, R, C, nx, ny) && heights[x][y] <= heights[nx][ny]) {
+      dfs(heights, V, R, C, nx, ny);
+      V[nx][ny] = true;
+    }
+  }
+  return;
+}
+
+vector<vector<int>> pacificAtlanticDFS(vector<vector<int>>& heights) {
+  const int R = heights.size();
+  const int C = heights[0].size();
+  vector<vector<int>> ans;
+  vector<bool> col(C, false);
+  vector<vector<bool>> vPac(R, col), vAlt(R, col);
+  for (int i = 0; i < R; i++) {
+    dfs(heights, vPac, R, C, i, 0);
+    dfs(heights, vAlt, R, C, i, C - 1);
+  }
+  for (int i = 0; i < C; i++) {
+    dfs(heights, vPac, R, C, 0, i);
+    dfs(heights, vAlt, R, C, R - 1, i);
+  }
+  for (int i = 0; i < R; i++) {
+    for (int j = 0; j < C; j++) {
+      if (vPac[i][j] && vAlt[i][j]) ans.push_back({i, j});
+    }
+  }
+  return ans;
+}
+
 int main() {
+  // BFS
   vector<vector<int>> h1 = {{1, 2, 2, 3, 5},
                             {3, 2, 3, 4, 4},
                             {2, 4, 5, 3, 1},
                             {6, 7, 1, 4, 5},
                             {5, 1, 1, 2, 4}};
-  cout << "ans1: " << pacificAtlantic(h1) << endl;
+  cout << "ans1 (BFS): " << pacificAtlantic(h1) << endl;
   vector<vector<int>> h2 = {{10, 10, 10}, {10, 1, 10}, {10, 10, 10}};
-  cout << "ans2: " << pacificAtlantic(h2) << endl;
+  cout << "ans2 (BFS): " << pacificAtlantic(h2) << endl;
   vector<vector<int>> h3 = {{13}, {4},  {19}, {10}, {1}, {11}, {5},  {17}, {3},
                             {10}, {1},  {0},  {1},  {4}, {1},  {3},  {6},  {13},
                             {2},  {16}, {7},  {6},  {3}, {1},  {9},  {9},  {13},
                             {10}, {9},  {10}, {6},  {2}, {11}, {17}, {13}, {0},
                             {19}, {7},  {13}, {3},  {9}, {2}};
-  cout << "ans3: " << pacificAtlantic(h3) << endl;
+  cout << "ans3 (BFS): " << pacificAtlantic(h3) << endl;
+  // DFS
+  cout << "ans1 (DFS): " << pacificAtlanticDFS(h1) << endl;
+  cout << "ans2 (DFS): " << pacificAtlanticDFS(h2) << endl;
+  cout << "ans3 (DFS): " << pacificAtlanticDFS(h3) << endl;
   return 0;
 }
