e9bbc0c6ede789a90fc0438b39b4821cf19962c1
bietdoikiem <minhlucky2408@gmail.com>
2022-06-09 16:49:23 +0700

    feat: add solution for Leetcode no. 143

---
+++ leetcode/143_Reorder_List/main.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define endl '\n'
+
+ostream& operator<<(ostream& os, pair<int, int> const& x) {
+  os << x.first << ", " << x.second;
+  return os;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, vector<T> const& x) {
+  os << "{ ";
+  for (auto y : x) os << y << " ";
+  return os << "}";
+}
+
+template <class T, size_t SIZE>
+void printArr(T const (&x)[SIZE]) {
+  cout << "{ ";
+  for (auto y : x) cout << y << " ";
+  cout << "}" << endl;
+}
+
+template <class... Args>
+auto& log(const Args&... args) {
+  return ((cerr << " " << args), ...) << '\n';
+}
+
+#define LOG(...)                     \
+  cerr << "[ " #__VA_ARGS__ " ] = "; \
+  log(__VA_ARGS__) << '\n'
+
+int dX[4] = {-1, 0, 1, 0};
+int dY[4] = {0, 1, 0, -1};
+
+/***** SOLUTION BELOW HERE *****/
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+// Unoptimized way: O(n) time - O (n) space
+void reorderList(ListNode* head) {
+  ListNode* cur = head;
+  stack<ListNode*> s;
+  while (cur != nullptr) s.push(cur), cur = cur->next;
+  int L = (int)s.size() / 2;
+  cur = head;
+  while (L > 0) {
+    ListNode *last = s.top(), *next = cur->next;
+    cur->next = last;
+    last->next = next;
+    cur = next;
+    s.pop(), L--;
+  }
+  cur->next = nullptr;
+}
+
+int main() { return 0; }
