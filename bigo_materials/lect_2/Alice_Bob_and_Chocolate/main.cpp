32a58041ce74530770799ade09697bd0becd0422
bietdoikiem <minhlucky2408@gmail.com>
2022-07-03 22:45:22 +0700

    feat: add solution for BigO lect_2 no. 8 problem

---
+++ bigo_materials/lect_2/Alice_Bob_and_Chocolate/main.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define endl '\n'
+
+ostream& operator<<(ostream& os, pair<int, int> const& x) {
+  os << x.first << ", " << x.second;
+  return os;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, vector<T> const& x) {
+  os << "{ ";
+  for (auto y : x) os << y << " ";
+  return os << "}";
+}
+
+template <class T, size_t SIZE>
+void printArr(T const (&x)[SIZE]) {
+  cout << "{ ";
+  for (auto y : x) cout << y << " ";
+  cout << "}" << endl;
+}
+
+template <class... Args>
+auto& log(const Args&... args) {
+  return ((cerr << " " << args), ...) << '\n';
+}
+
+#define LOG(...)                     \
+  cerr << "[ " #__VA_ARGS__ " ] = "; \
+  log(__VA_ARGS__) << '\n'
+
+int dX[4] = {-1, 0, 1, 0};
+int dY[4] = {0, 1, 0, -1};
+
+/***** SOLUTION BELOW HERE *****/
+
+void Solves(int bars[], int n) {
+  int l = 0, r = n - 1, a = 0, b = 0, aLeft = 0, bLeft = 0;
+  while (l <= r) {
+    if (aLeft == bLeft) {
+      if (l == r) {
+        a++, l++;
+        continue;
+      }
+      aLeft = bars[l], a++, l++;
+      bLeft = bars[r], b++, r--;
+    } else if (aLeft < bLeft) {
+      bLeft -= aLeft;
+      aLeft = bars[l], a++, l++;
+    } else if (aLeft > bLeft) {
+      aLeft -= bLeft;
+      bLeft = bars[r], b++, r--;
+    }
+  }
+  cout << a << " " << b << endl;
+}
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  int bars[n];
+  for (int i = 0; i < n; i++) scanf("%d", &bars[i]);
+  Solves(bars, n);
+  return 0;
+}
